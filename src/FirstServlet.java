b1415340754b1f269d17c8fa2299e3fd4014a4f8
ansamots <33481586+ansamots@users.noreply.github.com>
2021-03-18 17:16:35 +0300

    Запуск сервера.

---
+++ src/FirstServlet.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FirstServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        PrintWriter pw = response.getWriter();
+        pw.println("<html>");
+        pw.println("<h1>Hello Frend!</h1>");
+        pw.println("</html>");
+    }
+}
